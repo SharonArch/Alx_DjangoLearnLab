fbf55b0fdf1e4bfcdbfe7dce9c441eac30be8d92
sharon mogaka <mogakasharon5@gmail.com>
2025-11-09 01:43:43 +0300

    Fix queries to use .filter and direct get for ALX checks

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,13 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        return author.books.all()
+        return Book.objects.filter(author=author)
     except Author.DoesNotExist:
         return []
+
 
 def get_books_in_library(library_name):
     try:
@@ -14,14 +16,10 @@ def get_books_in_library(library_name):
     except Library.DoesNotExist:
         return []
 
+
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        return library.librarian
+        return Librarian.objects.get(library=library)
     except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
-
-if __name__ == "__main__":
-    print("Books by John Doe:", list(get_books_by_author("John Doe")))
-    print("Books in Central Library:", list(get_books_in_library("Central Library")))
-    print("Librarian for Central Library:", get_librarian_for_library("Central Library"))
