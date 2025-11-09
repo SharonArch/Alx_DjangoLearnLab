964c166972a180f17a41ed09401a12de1bb62757
sharon mogaka <mogakasharon5@gmail.com>
2025-11-09 04:53:47 +0300

    Fix Task 1: explicit imports for FBV and CBV

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,7 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Author, Book, Library
+from django.urls import path
+from .views import list_books, LibraryDetailView
 
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-
-from django.views.generic import DetailView
-from .models import Library
-
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-
+urlpatterns = [
+    path('list-books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
