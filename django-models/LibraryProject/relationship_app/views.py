6818a0d05360f65e936a756e90f45fed677262f7
sharon mogaka <mogakasharon5@gmail.com>
2025-11-16 10:49:02 +0300

    Implemented class-based view and fixed missing imports

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Author, Book, Library
+from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
