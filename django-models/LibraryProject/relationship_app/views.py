cd57919905b85506d9a24302156358a251225213
sharon mogaka <mogakasharon5@gmail.com>
2025-11-27 16:37:07 +0300

    Implementing user authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Library, Book
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
-# Function-based view
+# Function-based view (FBV) to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view for library details
+# Class-based view (CBV) to show details of a library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
